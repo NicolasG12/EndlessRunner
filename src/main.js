755dbfbfafeab8b402c68e63557a0bcb6e25024c
NicolasG12 <naguevar@ucsc.edu>
2022-04-23 19:10:44 -0700

    Fixed the skulls falling through the floors

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ let config = {
          debug: true
       }
    },
-   scene:  [Menu, Play]
+   scene:  [Menu, Play, GameOver]
 }
 
 
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -44,65 +44,22 @@ class Play extends Phaser.Scene {
             allowGravity: false,
             velocityX: -200
         });
-        this.physics.add.collider(this.player1, this.platformTopGroup);
-        this.physics.add.collider(this.player1, this.platformMidGroup);
-        this.physics.add.collider(this.player1, this.platformBotGroup);
-        // this.platformTop = [];
-        // this.platformTop1 = new Platform(this, 0, game.config.height / 4, 'platform', 0, platformSpeed).setOrigin(0, 0);;
-        // this.platformTop2 = new Platform(this, game.config.width / 2, game.config.height / 4, 'platform', 0, platformSpeed).setOrigin(0, 0);;
-        // this.platformTop3 = new Platform(this, game.config.width, game.config.height / 4, 'platform', 0, platformSpeed).setOrigin(0, 0);;
-        // this.platformTop4 = new Platform(this, game.config.width + (game.config.width / 2), game.config.height / 4, 'platform', 0, platformSpeed).setOrigin(0, 0);;
-        // this.platformTop.push(this.platformTop1, this.platformTop2, this.platformTop3, this.platformTop4);
-
-        // this.physics.add.collider(this.player1, this.platformTop);
-        // this.physics.add.collider(this.player1, this.platformTop2);
-        // this.physics.add.collider(this.player1, this.platformTop3);
-        // this.physics.add.collider(this.player1, this.platformTop4);
-        // Initialize Middle Platforms
-        // this.platformMid = [];
-        // this.platformMid1 = new Platform(this, 0, game.config.height / 2, 'platform', 0, platformSpeed).setOrigin(0, 0);;
-        // this.platformMid2 = new Platform(this, game.config.width / 2, game.config.height / 2, 'platform', 0, platformSpeed).setOrigin(0, 0);;
-        // this.platformMid3 = new Platform(this, game.config.width, game.config.height / 2, 'platform', 0, platformSpeed).setOrigin(0, 0);;
-        // this.platformMid4 = new Platform(this, game.config.width + (game.config.width / 2), game.config.height / 2, 'platform', 0, platformSpeed).setOrigin(0, 0);;
-        // this.platformMid.push(this.platformMid1, this.platformMid2, this.platformMid3, this.platformMid4);
 
 
-        // this.physics.add.collider(this.player1, this.platformMid2);
-        // this.physics.add.collider(this.player1, this.platformMid3);
-        // this.physics.add.collider(this.player1, this.platformMid4);
-        // Initialize Bot Platforms
-        // this.platformBot = [];
-        // this.platformBot1 = new Platform(this, 0, (game.config.height / 4) * 3, 'platform', 0, platformSpeed).setOrigin(0, 0);;
-        // this.platformBot2 = new Platform(this, game.config.width / 2, (game.config.height / 4) * 3, 'platform', 0, platformSpeed).setOrigin(0, 0);;
-        // this.platformBot3 = new Platform(this, game.config.width, (game.config.height / 4) * 3, 'platform', 0, platformSpeed).setOrigin(0, 0);;
-        // this.platformBot4 = new Platform(this, game.config.width + (game.config.width / 2), (game.config.height / 4) * 3, 'platform', 0, platformSpeed).setOrigin(0, 0);;
-        // this.platformBot.push(this.platformBot1, this.platformBot2, this.platformBot3, this.platformBot4);
-
-
-
-        
-        // this.physics.add.collider(this.player1, this.platformBot2);
-        // this.physics.add.collider(this.player1, this.platformBot3);
-        // this.physics.add.collider(this.player1, this.platformBot4);
         //add input keys
         keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
         keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
 
         //group for the enemy viruses
-        this.virusGroup = this.physics.add.group({
-            removeCallback: function(virus) {
-                virus.scene.virusPool.add(virus);
-            }
-        });
+        this.virusGroup = this.physics.add.group();
 
 
-        this.virusPool = this.physics.add.group({
-            removeCallback: function(virus) {
-                virus.scene.virusGroup.add(virus);
-            }
-        });
+        // this.virusPool = this.physics.add.group();
 
         //set the collision groups
+        this.physics.add.collider(this.player1, this.platformTopGroup);
+        this.physics.add.collider(this.player1, this.platformMidGroup);
+        this.physics.add.collider(this.player1, this.platformBotGroup);
         this.physics.add.collider(this.virusGroup, this.platformTopGroup);
         this.physics.add.collider(this.virusGroup, this.platformMidGroup);
         this.physics.add.collider(this.virusGroup, this.platformBotGroup);
@@ -111,20 +68,19 @@ class Play extends Phaser.Scene {
 
     addVirus(posY) {
         let virus;
-        console.log(this.virusPool.getLength());
-        if(this.virusPool.getLength()) {
-            virus = this.virusPool.getFirst();
-            virus.x = game.config.width;
-            virus.y = posY;
-            virus.active = true;
-            virus.visible = true;
-            this.virusPool.remove(virus);
-        }
-        else {
-            virus = new Virus(this, game.config.width + 100, posY, 'virus').setOrigin(0, 0);
-            virus.setImmovable(true);
-            this.virusGroup.add(virus);
-        }
+        // if(this.virusPool.getLength()) {
+        //     virus = this.virusPool.getFirst();
+        //     virus.x = game.config.width;
+        //     virus.y = posY;
+        //     console.log(virus.y);
+        //     virus.active = true;
+        //     virus.visible = true;
+        //     this.virusPool.remove(virus);
+        // }
+        // else {
+        virus = new Virus(this, game.config.width + 100, posY, 'virus').setOrigin(0, 0);
+        this.virusGroup.add(virus);
+        //}
         this.virusGroup.setVelocityX(virusSpeed);
     }
 
@@ -133,24 +89,11 @@ class Play extends Phaser.Scene {
         this.background.tilePositionX += 4;
         // Update Player
         this.player1.update();
-        // Update Top Platforms
-        // this.platformTop1.update();
-        // this.platformTop2.update();
-        // this.platformTop3.update();
-        // this.platformTop4.update();
-        // Update Middle Platforms
-        // this.platformMid1.update();
-        // this.platformMid2.update();
-        // this.platformMid3.update();
-        // this.platformMid4.update();
-        // Update Bottom Platforms
-        // this.platformBot1.update();
-        // this.platformBot2.update();
-        // this.platformBot3.update();
-        // this.platformBot4.update();
-        let rand = Math.floor(Math.random() * 300);
-        if(rand < 3) {
-            this.addVirus(rand * differenceY + 90);
+        
+        if(Math.floor(Math.random() * 100) == 17) {
+            let lane = (Math.floor(Math.random() * 3))
+            this.addVirus(lane * differenceY + 90);
+            console.log(lane * differenceY + 90);
         }
         this.virusGroup.getChildren().forEach((virus) => {
             if(virus.x <= 0 - virus.width) {
