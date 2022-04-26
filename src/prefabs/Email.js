af9bd473fbe260f60bcfcf72a5c7900f3ea6a5a3
Daniel <dolaes@ucsc.edu>
2022-04-25 22:27:23 -0700

    Tile Platforms & Elimination of Platform and Virus js Files

--- src/prefabs/Email.js
+++ src/prefabs/Email.js
@@ -7,10 +7,12 @@ class Email extends Phaser.Physics.Arcade.Sprite {
     }
 
     update() {
-        if(Phaser.Input.Keyboard.JustDown(keyUP) && this.y > 110) {
+        // If the player is below the top lane, they can move up
+        if(Phaser.Input.Keyboard.JustDown(keyUP) && this.y > game.config.height/4) {
             this.y -= differenceY;
         }
-        if(Phaser.Input.Keyboard.JustDown(keyDOWN) && this.y < 390) {
+        // If the player is above the bottom lane, they can move down
+        if(Phaser.Input.Keyboard.JustDown(keyDOWN) && this.y < game.config.height/2) {
             this.y += differenceY;
         }
     }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,125 +5,112 @@ class Play extends Phaser.Scene {
 
     preload() {
         this.load.image('background', './assets/tempBackground.png');
-        this.load.image('platform', './assets/tempPlatform.png');
-        this.load.spritesheet('virus', './assets/Skull Animation Draft.png', {frameWidth: 48, frameHeight: 48, startFrame: 0, endFrame: 4});
-        this.load.spritesheet('email', './assets/Mail-E Animation Draft.png', {frameWidth: 48, frameHeight: 48, startFrame: 0, endFrame: 2});
+        this.load.image('platformTile', './assets/tempPlatTile.png');
+        this.load.spritesheet('virus', './assets/Skull Animation Draft.png', { frameWidth: 48, frameHeight: 48, startFrame: 0, endFrame: 4 });
+        this.load.spritesheet('email', './assets/Mail-E Animation Draft.png', { frameWidth: 48, frameHeight: 48, startFrame: 0, endFrame: 2 });
     }
 
     create() {
+        // Variable Initialization
+        this.tileSize = 20;
         this.spawnTime = 1000;
+        this.scrollSpeed = 4;
+
         // Initialize Background
         this.background = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'background').setOrigin(0, 0);
 
+        // Initialize Platform Group
+        this.platforms = this.add.group();
+
+        // Initialize Top Platform
+        for (let i = 0; i < game.config.width; i += this.tileSize) {
+            let topTile = this.physics.add.sprite(i, game.config.height / 4 - this.tileSize, 'platformTile').setOrigin(0, 0);
+            topTile.body.immovable = true;
+            topTile.body.allowGravity = false;
+            this.platforms.add(topTile);
+        }
+
+        // Initialize Middle Platform
+        for (let i = 0; i < game.config.width; i += this.tileSize) {
+            let midTile = this.physics.add.sprite(i, game.config.height / 2 - this.tileSize, 'platformTile').setOrigin(0, 0);
+            midTile.body.immovable = true;
+            midTile.body.allowGravity = false;
+            this.platforms.add(midTile);
+        }
+
+        // Initialize Middle Platform
+        for (let i = 0; i < game.config.width; i += this.tileSize) {
+            let botTile = this.physics.add.sprite(i, ((game.config.height / 4) * 3) - this.tileSize, 'platformTile').setOrigin(0, 0);
+            botTile.body.immovable = true;
+            botTile.body.allowGravity = false;
+            this.platforms.add(botTile);
+        }
+
         // Initialize Player
-        this.player1 = new Email(this, playerX, game.config.height / 2 - 100, 'email').setOrigin(0, 0);
-
-        // Initialize Top Platforms
-
-        this.platformTopGroup = this.physics.add.group({
-            classType: Platform,
-            key: 'platform',
-            repeat: 3,
-            setXY: {x: 0, y: game.config.height/4, stepX: game.config.width/2},
-            immovable: true,
-            allowGravity: false,
-            velocityX: -200
-        });
-        this.platformMidGroup = this.physics.add.group({
-            classType: Platform,
-            key: 'platform',
-            repeat: 3,
-            setXY: {x: 0, y: game.config.height/2, stepX: game.config.width/2},
-            immovable: true,
-            allowGravity: false,
-            velocityX: -200
-        });
-        this.platformBotGroup = this.physics.add.group({
-            classType: Platform,
-            key: 'platform',
-            repeat: 3,
-            setXY: {x: 0, y: (game.config.height / 4) * 3, stepX: game.config.width/2},
-            immovable: true,
-            allowGravity: false,
-            velocityX: -200
-        });
-
-
-        //add input keys
-        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-        keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
-
-        //group for the enemy viruses
-        this.virusGroup = this.physics.add.group();
-
-
-        // this.virusPool = this.physics.add.group();
-
-        //create an animation for the virus
-        this.anims.create({
-            key: 'virusAnimation',
-            frames: this.anims.generateFrameNumbers('virus', {start: 0, end: 4, first: 0}),
-            frameRate: 15,
-            repeat: -1
-        });
+        this.player1 = new Email(this, playerX, game.config.height / 2 - 68, 'email').setOrigin(0, 0);
+        // Create an animation for the player
         this.anims.create({
             key: 'emailAnimation',
-            frames: this.anims.generateFrameNumbers('email', {start: 0, end: 4, first: 0}),
+            frames: this.anims.generateFrameNumbers('email', { start: 0, end: 4, first: 0 }),
             frameRate: 15,
             repeat: -1
-        })
-
-        //set the collision groups
-        this.physics.add.collider(this.player1, this.platformTopGroup);
-        this.physics.add.collider(this.player1, this.platformMidGroup);
-        this.physics.add.collider(this.player1, this.platformBotGroup);
-        this.physics.add.collider(this.virusGroup, this.platformTopGroup);
-        this.physics.add.collider(this.virusGroup, this.platformMidGroup);
-        this.physics.add.collider(this.virusGroup, this.platformBotGroup);
-        this.physics.add.overlap(this.player1, this.virusGroup, this.playerHit, null, this);
-
-        //spawn enemies every second
-        setInterval( () => {
-            let lane = (Math.floor(Math.random() * 3))
-            this.addVirus(lane * differenceY + 90);
-        }, this.spawnTime);
+        });
         this.player1.play('emailAnimation');
 
-    }
 
-    addVirus(posY) {
-        let virus;
-        virus = new Virus(this, game.config.width, posY, 'virus').setOrigin(0, 0);
-        this.virusGroup.add(virus);
-        this.virusGroup.setVelocityX(virusSpeed);
-        virus.play('virusAnimation');
+        // Group for the enemy viruses
+        this.viruses = this.physics.add.group();
+        // Create an animation for the virus
+        this.anims.create({
+            key: 'virusAnimation',
+            frames: this.anims.generateFrameNumbers('virus', { start: 0, end: 4, first: 0 }),
+            frameRate: 15,
+            repeat: -1
+        });
+
+        // Spawn enemies every second
+        setInterval(() => {
+            let lane = (Math.floor(Math.random() * 3))
+            let virus = this.physics.add.sprite(game.config.width, (lane * differenceY) + 85, 'virus').setOrigin(0, 0);
+            this.viruses.add(virus);
+            virus.play('virusAnimation');
+            this.viruses.setVelocityX(virusSpeed);
+        }, this.spawnTime);
+        
+        // Collision
+        this.physics.add.collider(this.player1, this.platforms);
+        this.physics.add.collider(this.viruses, this.platforms);
+        this.physics.add.overlap(this.player1, this.viruses, this.playerHit, null, this);
+
+        // Add input keys
+        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
     }
 
     update() {
         // Update Background
-        this.background.tilePositionX += 4;
+        this.background.tilePositionX += this.scrollSpeed;
+        this.platforms.getChildren().forEach((tile) => {
+            tile.setX(tile.x - this.scrollSpeed);
+            this.physics.world.wrap(tile, 0);
+        }, this);
+
         // Update Player
         this.player1.update();
-        this.virusGroup.getChildren().forEach((virus) => {
-            if(virus.x <= 0 - virus.width) {
+
+        // Update Viruses
+        this.viruses.getChildren().forEach((virus) => {
+            if (virus.x <= 0 - virus.width) {
                 virus.destroy();
             }
         }, this);
-
-        this.platformTopGroup.getChildren().forEach((platform) => {
-            platform.update();
-        }, this);
-        this.platformMidGroup.getChildren().forEach((platform) => {
-            platform.update();
-        }, this);
-        this.platformBotGroup.getChildren().forEach((platform) => {
-            platform.update();
-        }, this) 
     }
 
+    // Player Hit Function
     playerHit(player, virus) {
-        this.virusGroup.killAndHide(virus);
-        this.virusGroup.remove(virus);
+        this.viruses.killAndHide(virus);
+        this.viruses.remove(virus);
+
         // Go to Game Over Screen
         this.scene.start("gameOver");
         console.log("Game over");
