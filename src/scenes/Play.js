df18bc4e77b2ec9a702798cbd7d2045973936d1b
NicolasG12 <naguevar@ucsc.edu>
2022-05-01 14:47:37 -0700

    Added paralax scrolling

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,7 +8,7 @@ class Play extends Phaser.Scene {
         this.load.image('platformTile', '01_tile.png');
         this.load.image('background', '01_background.png');
         this.load.image('particle', '01_particle.png');
-        // this.load.image('numBackground', './assets/numberBackground.png');
+        this.load.image('background2', '02_background.png');
         this.load.spritesheet('controls', '01_controls.png', { frameWidth: 96, frameHeight: 48, startFrame: 0, endFrame: 3 });
         this.load.spritesheet('virus1', '01_virus.png', { frameWidth: 48, frameHeight: 48, startFrame: 0, endFrame: 3 });
         this.load.spritesheet('virus2', '02_virus.png', { frameWidth: 48, frameHeight: 48, startFrame: 0, endFrame: 3 });
@@ -32,7 +32,7 @@ class Play extends Phaser.Scene {
         // Variable Initialization
         this.tileSize = 20;
         this.spawnTime = 1000;
-        this.scrollSpeed = 4;
+        this.scrollSpeed = 2;
         this.virusSpeed1 = -250;
         this.virusSpeed2 = -400;
         this.platformSpeed = 4;
@@ -42,7 +42,7 @@ class Play extends Phaser.Scene {
 
         // Initialize Background
         this.background = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'background').setOrigin(0, 0);
-        // this.numBackground = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'numBackground').setOrigin(0, 0);
+        this.background2 = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'background2').setOrigin(0, 0);
 
         // Initialize Platform Group
         this.platforms = this.add.group();
@@ -246,7 +246,7 @@ class Play extends Phaser.Scene {
         }
         // Update Background
         this.background.tilePositionX += this.scrollSpeed;
-        // this.numBackground.tilePositionX += this.scrollSpeed * 1.5;
+        this.background2.tilePositionX += this.scrollSpeed * 1.5;
         this.platforms.getChildren().forEach((tile) => {
             tile.setX(tile.x - this.scrollSpeed);
             this.physics.world.wrap(tile, 0);
